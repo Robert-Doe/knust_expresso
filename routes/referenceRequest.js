d30efaa75848a05f1df47ee3cc686fd90be3dcd5
Robert Doe <robertdoe60@gmail.com>
2023-07-29 07:57:01 -0500

    Update this evening

--- routes/referenceRequest.js
+++ routes/referenceRequest.js
@@ -3,9 +3,15 @@ const express = require('express');
 const router = express.Router();
 const ReferenceRequest = require('../models/ReferenceRequest');
 const Request = require('../models/Request');
+const Lecturer = require('../models/Lecturer');
 const multer = require('multer'); // Middleware for handling file uploads
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
+const nodemailer = require('nodemailer');
+
+const API_KEY = 'Nk3SqCe3G3FFXaAlZDKxaxvv5 '; // Replace with your mNotify API key
+const SENDER_ID = 'Xpresso'; // Replace with your mNotify sender ID
 
 const uploadDirectory = 'uploads/reference';
 
@@ -24,6 +30,67 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + uniqueSuffix + ext);
     },
 });
+
+
+
+async function sendSMS(recipient, message) {
+    try {
+        const apiUrl = 'https://apps.mnotify.net/smsapi';
+        const params = {
+            key: API_KEY,
+            to: recipient,
+            msg: message,
+            sender_id: SENDER_ID,
+        };
+
+        const response = await axios.get(apiUrl, { params });
+
+        if (response.data.status === 'success') {
+            console.log('SMS sent successfully.');
+            return true;
+        } else {
+            console.error('Failed to send SMS:', response.data);
+            return false;
+        }
+    } catch (error) {
+        console.error('Error sending SMS:', error);
+        return false;
+    }
+}
+
+
+
+// Function to send an email using NodeMailer
+const sendEmail = async (lecturerEmail, subject, text) => {
+    try {
+        // Create a transporter object using your email service provider's SMTP settings
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.titan.email',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'knustexpresso@codeden.org',
+                pass: 'bob@Cumulus#4717'
+            }
+        });
+
+        // Define the email options
+        const mailOptions = {
+            from: 'KNUST Xpresso <knustexpresso@codeden.org>',
+            to: lecturerEmail,
+            subject,
+            text,
+        };
+
+        // Send the email
+        await transporter.sendMail(mailOptions);
+
+        console.log('Email sent successfully.');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
+
 
 router.get('/lecturer-reference/:lecturerId', async (req, res) => {
 
@@ -62,6 +129,20 @@ router.post('/', upload.fields([{name: 'cvFile'}, {name: 'transcriptFile'}]), as
             transcriptUrl,
             resumeUrl
         });
+
+        const lecturer = await Lecturer.findByPk(lecturerId)
+        // After successfully creating the reference request, send an SMS notification to the lecturer
+        const lecturerPhoneNumber = lecturer.phoneNumber; // Replace with the lecturer's phone number
+        const message = `Hello, you have a new request for a recommendation letter. Please check your dashboard for details.\nReference Request ID: ${referenceRequest.id}\nStudent ID: ${studentId}`; // Customize the SMS message as needed
+        await sendSMS(lecturerPhoneNumber, message);
+
+        // After successfully creating the reference request, send an email to the lecturer
+        const lecturerEmail = lecturer.email; // Replace with the lecturer's email address
+        const subject = 'New Request for Recommendation Letter';
+        const messageEmail = `Hello,\n\nYou have a new request for a recommendation letter. Please check your dashboard for details.\n\nReference Request ID: ${referenceRequest.id}\nStudent ID: ${studentId}`; // Customize the email message as needed
+
+        await sendEmail(lecturerEmail, subject, messageEmail);
+
 
         // Save the referenceRequest to the database
         const request = await referenceRequest.save();
